15f80e4a2ad7266999a4260f5856b1116858d562
Clara Le Roux <clara.mg.leroux@gmail.com>
2019-11-26 14:51:13 +0100

    seeds started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+GivenLike.destroy_all
+Job.destroy_all
+User.destroy_all
+
+
+puts 'Creating users...'
+clara = User.new(ig_user_id: "clara@lr.com")
+clara.save!
+
+
+
+puts 'Creating jobs...'
+techno = Job.new(
+  image_url: "www.instagram/sogadgets/3168419555",
+  caption: "I love gadgets",
+  hashtag: "#technology",
+  user_id: clara.id,
+  user_ig_media_id: "1234567894561231236_33215652",
+  likes_received: "5",
+  launched_at: "2019-11-18",
+  stopped_at: "2019-11-18"
+)
+techno.save!
