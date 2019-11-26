7032ea71344ea91b0e4aa18d87151284cbb3a04c
Nicholas Hendrickson <Nlhendrickson@hotmail.com>
2019-11-26 14:33:03 +0100

    controller& view for user & jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,40 @@
 class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @job = Job.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @job = Job.new(job_params)
+    @job.user = @user
+    @job.save
+    redirect_to user_jobs_path(@user)
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to user_jobs_path(@user)
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:image_url, :caption, :hastag, :user_id, :likes_received, :launched_at, :stopped_at)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:ig_user_id)
+  end
 end
