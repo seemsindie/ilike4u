bd18af060ea6e01ab62c40408684ad7ef157a79b
alessandro buccarelli <abuccarelli@hotmail.com>
2019-11-26 10:45:57 +0100

    created controllers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users, except: :delete do
+    resources :jobs, except: :delete do
+     resources :given_likes, only: [:index]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
