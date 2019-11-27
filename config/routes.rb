5ce75a100d77ac518b2a237cec733b7be734188f
Nicholas Hendrickson <Nlhendrickson@hotmail.com>
2019-11-27 10:34:19 +0100

    added methods for GivenLke Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, except: :delete do
     resources :jobs, except: :delete do
-     resources :given_likes, only: [:index]
+     resources :given_likes, :delete
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
