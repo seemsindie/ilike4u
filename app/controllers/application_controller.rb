77ee5b7e68dc5d1a60ff7e8741dfbf63233fa0fd
Clara Le Roux <clara.mg.leroux@gmail.com>
2019-11-28 16:50:42 +0100

    css navbar working better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    new_user_job_path(resource.id)
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_job_path(resource.id)
+  end
 end
