f3834ebc7a2f12f4cc015f315a416a5419aa2e72
alessandro buccarelli <abuccarelli@hotmail.com>
2019-11-29 15:33:20 +0100

    automation working in localhost

--- app/bot2.rb
+++ app/bot2.rb
@@ -3,13 +3,11 @@ require 'pry' # Ruby REPL
 require 'selenium-webdriver'
 require 'chromedriver-helper'
 
-class Bot
-  def self.insta_bot(job)
     like_counter = 0
     photo_liked = []
     user_liked = []
     user_hashtags = []
-    # hashtag = "cat"
+    hashtag = "cat"
 
     # Open Browser, Navigate to Login page
     browser = Watir::Browser.new :chrome, switches: ['--incognito']
@@ -17,8 +15,8 @@ class Bot
     browser.goto "instagram.com/accounts/login/"
     sleep(3.03)
     puts "Logging in..."
-    browser.text_field(name: "username").set "#{job.instagram_username}"
-    browser.text_field(name: "password").set "#{job.instagram_password}"
+    browser.text_field(name: "username").set "petpawer"
+    browser.text_field(name: "password").set "Jaimelepoulet1."
     sleep(3.09)
     browser.button(class: ['sqdOP', 'L3NKy', 'y3zKF']).click
     sleep(3.12)
@@ -30,7 +28,7 @@ class Bot
     2.times do
       puts "--------- #{Time.now} Starting loop----------"
       # goes on the hashtag
-      browser.goto "https://www.instagram.com/explore/tags/#{job.hashtag}"
+      browser.goto "https://www.instagram.com/explore/tags/#{hashtag}"
       sleep(3.03)
       # goes on the picture
       browser.divs(class: ['_9AhH0'])[9].click
@@ -38,8 +36,10 @@ class Bot
       photo_liked << browser.url
       sleep(1)
       user_liked << browser.h2(class: ['_6lAjh']).text
+
       sleep(3.09)
       user_hashtags << browser.div(class: ['C4VMK']).span.text.match(/#.+/).to_s.split(" ")
+
 
       # checked if liked
       if browser.span(class: ['glyphsSpriteHeart__outline__24__grey_9', 'u-__7']).exists?
@@ -54,5 +54,8 @@ class Bot
       puts "--------- #{Time.now} Closing loop----------"
       sleep(45)
     end
-  end
-end
+puts "You liked  #{like_counter}"
+puts "You liked the following url : #{photo_liked}"
+puts "You liked the following profiles : #{user_liked}"
+puts " #{user_hashtags} "
+
