9d3b02eca4650c787df316218ceac32debda4518
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2021-07-21 14:12:13 +0200

    register shadowbaning page frontend

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
 
   get 'register/final' => 'pages#register_final'
 
+  get 'register/shadow' => 'pages#register_shadowbaning'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
